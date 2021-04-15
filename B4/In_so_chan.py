da94c199541e3ed55871c6782cf95f9f90889a03
khanhnt99 <ntkhanh99.vn@gmail.com>
2021-04-15 21:31:04 +0700

    done B4

---
+++ B4/In_so_chan.py
@@ -0,0 +1,12 @@
+for i in range(1,11):
+    if i % 2 ==0:
+        print(i)
+print('--------------------')
+i=1
+while i<11:
+    if i%2==0:
+        print(i)
+    i=i+1
+print('---------------------')
+
+
---
+++ B4/project-euler-16.py
@@ -0,0 +1,6 @@
+x=2**1000
+mysum = 0
+for i in str(x):
+    # vi string co the lay tung phan tu
+    mysum=mysum + int(i)
+print(mysum)
