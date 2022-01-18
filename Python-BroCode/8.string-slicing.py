56f38a02bb0e7334c5190a36d03fbf7169e7b223
Your Name <you@example.com>
2022-01-18 22:49:19 +0700

    update string slicing
    Signed-off-by: Your Name <you@example.com>

---
+++ Python-BroCode/8.string-slicing.py
@@ -0,0 +1,20 @@
+name = "Bro Code"
+
+# first_name = name[:3]
+# last_name = name[4:]
+# funky_name = name[::2]
+# reversed_name = name[::-1]
+# # start:stop:step
+
+# print(first_name)
+# print(last_name)
+# print(funky_name)
+# print(reversed_name)
+
+website1 = "http://google.com"
+website2 = "http://wikipedia.com"
+
+slice = slice(7,-4)
+print(slice)
+print(website1[slice])
+print(website2[slice])
