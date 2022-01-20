6db2c0065c979912cd00acf5077a59cdbdb9d85b
khanhnt99 <ntkhanh99.vn@gmail.com>
2022-01-20 17:29:43 +0700

    updae nested loops
    Signed-off-by: khanhnt99 <ntkhanh99.vn@gmail.com>

---
+++ Python-BroCode/12.for-loops.py
@@ -0,0 +1,13 @@
+# for i in range(10):
+#     print(i+1)
+
+# for i in range(50,100+1,2):
+#     print(i)
+
+# for i in "Bro Code":
+    # print(i)
+import time
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(1)
+print("Happy New Year!")
