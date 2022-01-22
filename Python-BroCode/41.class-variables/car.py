fc29f824f4969560a7812183e8427dda1cd2637d
Your Name <you@example.com>
2022-01-22 11:38:49 +0700

    doing multilevel inheritance
    Signed-off-by: Your Name <you@example.com>

---
+++ Python-BroCode/41.class-variables/car.py
@@ -0,0 +1,9 @@
+class Car:
+
+    wheels = 4
+
+    def __init__(self,make,model,year,color):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.color = color
---
+++ Python-BroCode/41.class-variables/main.py
@@ -0,0 +1,10 @@
+from car import Car
+
+car_1 = Car("Chevy", "Corvette", 2021, "blue")
+car_2 = Car("Ford", "Mustang", 2022, "red")
+
+# car_1.wheels = 2
+Car.wheels = 2
+
+print(car_1.wheels)
+print(car_2.wheels)
