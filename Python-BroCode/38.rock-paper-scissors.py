fd520ac6575d4bdb3cea306a488d052fa7243293
Your Name <you@example.com>
2022-01-22 11:07:06 +0700

    done 39 quiz game
    Signed-off-by: Your Name <you@example.com>

---
+++ Python-BroCode/38.rock-paper-scissors.py
@@ -0,0 +1,46 @@
+import random
+while True:
+    choices = ['rock', 'paper', 'scissors']
+
+    computer = random.choice(choices)
+    player = None
+
+    while player not in choices:
+        player = input("rock, paper or scissors?: " ).lower()
+
+    if player == computer:
+        print("computer: ",computer)
+        print("player: ",player)
+        print("Tie!")
+    elif player == "rock":
+        if computer == "paper":
+            print("computer: ",computer)
+            print("player: ",player)
+            print("You lose!")
+        if computer == "scissors":
+            print("computer: ",computer)
+            print("player: ",player)
+            print("You win!")
+    elif player == "scissors":
+        if computer == "paper":
+            print("computer: ",computer)
+            print("player: ",player)
+            print("You win!")
+        if computer == "rock":
+            print("computer: ",computer)
+            print("player: ",player)
+            print("You lose!")
+    elif player == "paper":
+        if computer == "rock":
+            print("computer: ",computer)
+            print("player: ",player)
+            print("You win!")
+        if computer == "scissors":
+            print("computer: ",computer)
+            print("player: ",player)
+            print("You lose!")
+    play_again = input("Play again? (Yes/No): ").lower()
+
+    if play_again != 'yes':
+        break
+print("Bye")
