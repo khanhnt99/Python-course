00c738bae4f32b0bc9b90d8fe4811f16097e495d
Your Name <you@example.com>
2022-01-23 23:47:42 +0700

    update abstract classes
    Signed-off-by: Your Name <you@example.com>

--- Python-BroCode/48.abstract-classes.py
+++ Python-BroCode/48.abstract-classes.py
@@ -1,19 +1,37 @@
-class Vehicle:
+from abc import ABC,abstractmethod
+
+class Vehicle(ABC):
+
+    @abstractmethod
     def go(self):
+        pass
+    
+    @abstractmethod
+    def stop(self):
         pass
 
 class Car(Vehicle):
     def go(self):
         print("You drive the car")
 
+    def stop(self):
+        print("This car is stopped")
+
 class Motorcycle(Vehicle):
     def go(self):
         print("You ride motorcycle")
 
-vehicle = Vehicle()
+    def stop(self):
+        print("This motorcycle is stopped")
+
+# vehicle = Vehicle()
 car = Car()
 motorcycle = Motorcycle()
 
-vehicle.go()
+# vehicle.go()
+
 car.go()
+car.stop()
+
 motorcycle.go()
+motorcycle.stop()
